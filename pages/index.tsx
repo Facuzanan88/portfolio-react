b04c2c1e3fd3f8c61f0ae167b60115f69bbaea4c
Facuzanan88 <facundozanandrea@gmail.com>
2023-03-08 18:58:01 -0300

    my portfolio

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
 
+import { BsFillMoonStarsFill } from "react-icons/bs";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
@@ -12,10 +14,23 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
+      <main className="bg-white px-10">
         <section className="min-h-screen">
-          <nav>
-            <h1>developedbyed</h1>
+          <nav className="py-10 mb-12 flex justify-between">
+            <h1 className="text-xl font-burtons">developed by Piki</h1>
+            <ul className="flex items-center">
+              <li>
+                <BsFillMoonStarsFill className="cursor-pointer text-2xl" />
+              </li>
+              <li>
+                <a
+                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
+                  href="#"
+                >
+                  Resume
+                </a>
+              </li>
+            </ul>
           </nav>
         </section>
       </main>
